42c6ae663b24804f52ae79066b1e3ecbeeae3c7a
SuvasD <suvasdandu@gmail.com>
2025-07-28 11:36:41 -0700

    Add files via upload

---
+++ logicaloper.py
@@ -0,0 +1,14 @@
+a=0
+b=12
+c=10
+if a and b and c:
+    print("They're all true")
+else:
+    print("At least one of the numbers have false")
+
+if a>0 or b>0:
+    print("The number is positive")
+elif b>0 or c>0:
+    print("The number is greater than zero")
+else:
+    print("the number is negative")
